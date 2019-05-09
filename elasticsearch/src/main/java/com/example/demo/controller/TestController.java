72deee081c65818445ac844fb5011d17ca49bf78
yinjie <yinjie@jusfoun.com>
2019-05-09 13:49:31 +0800

    build step3 测试添加数据

--- elasticsearch/src/main/java/com/example/demo/controller/TestController.java
+++ elasticsearch/src/main/java/com/example/demo/controller/TestController.java
@@ -1,41 +1,30 @@
 package com.example.demo.controller;
 
-import com.example.demo.Entity.Employee;
+import com.alibaba.fastjson.JSON;
+import com.example.demo.Entity.Article;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.query.AliasQuery;
-import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("es")
 public class TestController {
-@Autowired
-    private  TransportClient  transportClient;
-
-    //省去service逻辑
     @Autowired
-    private ElasticsearchTemplate elasticsearchTemplate;
+    private TransportClient transportClient;
+
     @RequestMapping("add")
     public String add() {
-        Employee employee = new Employee();
-        employee.setFirstName("xuxu");
-        employee.setLastName("zh");
-        employee.setAge(26);
-        employee.setAbout("i am in peking");
-        transportClient.prepareIndex("cc","cc","1").setSource(employee.toString(), XContentType.JSON);
+        Article employee = new Article();
+        employee.setAttUrl("123");
+        employee.setContext("54654");
+        employee.setGeographicalLocation("schj");
+        employee.setGrade("123321");
+        transportClient.prepareIndex("cc1", "cc1").setSource(JSON.toJSONString(employee),XContentType.JSON).get();
         System.err.println("add a obj");
         return "success";
     }
-
-    @RequestMapping("get")
-    public Object get() {
-        return  transportClient.prepareGet("cc","cc","1").get().getSource();
-    }
-
 
 
 }
